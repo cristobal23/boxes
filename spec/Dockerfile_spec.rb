851985eac93e287820799fd9e9bc096862349d87
Cristobal Villarroel <cristobal23@gmail.com>
2017-10-16 03:57:50 +0000

    Add first test

---
+++ spec/Dockerfile_spec.rb
@@ -0,0 +1,21 @@
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :linux
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  it "installs the right version of Alpine" do
+    expect(os_version).to include("3.4.6")
+  end
+
+  def os_version
+    command("cat /etc/alpine-release").stdout
+  end
+end
+
