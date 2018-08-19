c48d54d56e1fd718baaa925ceb33957230986c3f
Cris <cristobal23@users.noreply.github.com>
2018-08-18 23:10:22 -0700

    Test latest alpine (#5)
    * Rename dockerfile test
    
    * Format test with black
    
    * Add docstrings
    
    * Refactor variables
    
    * Add test for alpine 3.7
    
    * Allow the test to pass
    
    * Add test for latest alpine version
    
    * Allow latest test to pass
    
    * Add test for alpine 3.8
    
    * Allow test to pass

---
+++ test_dockerfile_a36x.py
@@ -0,0 +1,50 @@
+import os
+import pytest
+import subprocess
+import testinfra
+
+
+DOCKER_TAG = '3.6'
+
+
+@pytest.fixture(scope='session')
+def host(request):
+    """Build Docker containers with Testinfra by overloading the host fixture."""
+    username = os.environ['DOCKER_USERNAME']
+    image_name = username + '/alpine:' + DOCKER_TAG
+
+    # build local ./Dockerfile
+    subprocess.check_call(
+        [
+            'docker',
+            'build',
+            '-f',
+            'Dockerfile',
+            '-t',
+            image_name,
+            '--build-arg',
+            'TAG=' + DOCKER_TAG,
+            '.',
+        ]
+    )
+
+    # run a container
+    docker_id = (
+        subprocess.check_output(
+            ['docker', 'run', '-d', image_name, 'tail', '-f', '/dev/null']
+        )
+        .decode()
+        .strip()
+    )
+
+    # return a testinfra connection to the container
+    yield testinfra.get_host("docker://" + docker_id)
+
+    # at the end of the test suite, destroy the container
+    subprocess.check_call(['docker', 'rm', '-f', docker_id])
+
+
+def test_myimage(host):
+    """Test the built Docker container."""
+    # 'host' now binds to the container
+    assert host.check_output('cat /etc/alpine-release') == '3.6.2'
