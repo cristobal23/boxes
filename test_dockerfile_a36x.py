f393b78988d5093868d0e2b9fb2f2cf09d84838c
Cristobal Villarroel <cristobal23@gmail.com>
2018-08-18 20:38:13 -0700

    Format test with black

--- test_dockerfile_a36x.py
+++ test_dockerfile_a36x.py
@@ -3,17 +3,31 @@ import subprocess
 import testinfra
 
 
-# scope='session' uses the same container for all the tests;
-# scope='function' uses a new container per test function.
-
 @pytest.fixture(scope='session')
 def host(request):
     # build local ./Dockerfile
-    subprocess.check_call(['docker', 'build', '-f', 'Dockerfile', '-t', 'cristobal23/alpine:3.6', '--build-arg', 'TAG=3.6', '.'])
+    subprocess.check_call(
+        [
+            'docker',
+            'build',
+            '-f',
+            'Dockerfile',
+            '-t',
+            'cristobal23/alpine:3.6',
+            '--build-arg',
+            'TAG=3.6',
+            '.',
+        ]
+    )
 
     # run a container
-    docker_id = subprocess.check_output(
-        ['docker', 'run', '-d', 'cristobal23/alpine:3.6', 'tail', '-f', '/dev/null']).decode().strip()
+    docker_id = (
+        subprocess.check_output(
+            ['docker', 'run', '-d', 'cristobal23/alpine:3.6', 'tail', '-f', '/dev/null']
+        )
+        .decode()
+        .strip()
+    )
 
     # return a testinfra connection to the container
     yield testinfra.get_host("docker://" + docker_id)
@@ -25,4 +39,3 @@ def host(request):
 def test_myimage(host):
     # 'host' now binds to the container
     assert host.check_output('cat /etc/alpine-release') == '3.6.2'
-
